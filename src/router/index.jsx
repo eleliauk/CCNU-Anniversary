1c53d17e6376110841787576a303a4d641270c7e
lilith xia <xiazhizhizhi@gmail.com>
2024-10-03 05:21:39 +0800

    chore: change router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,3 @@
-import { Navigate } from "react-router-dom";
-import Cutt from "../components/cutavatar";
 import Main from "../components/main";
 import React from "react";
 const router = [
@@ -7,10 +5,5 @@ const router = [
         path:'/',
         element: <Main/>
     },
-    {
-        path:'/editImage',
-        element:<Cutt/>
-    },
-
 ]
 export default router
